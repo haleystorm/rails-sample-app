ad6ac3355b8d7bec4a7eeb01e01f658ab8859061
Haley Quentmeyer <haleystorm@users.noreply.github.com>
2021-03-14 19:07:14 -0700

    6.2 Associating Models

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,5 @@
 class Article < ApplicationRecord
+  has_many :comments
   # min length 5 provided by tutorial
   # max length 78 from RFC 2822 recommendation of no more than 78 characters
   # in the subject header
